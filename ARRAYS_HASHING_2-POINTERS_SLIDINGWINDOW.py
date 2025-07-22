90044307592ada054f1a757600a688c35cb20ef3
Karthikeya Polakonda <98509169+karthikeyapolakonda@users.noreply.github.com>
2025-07-22 14:42:14 +0530

    Update ARRAYS_HASHING_2-POINTERS_SLIDINGWINDOW.py

--- ARRAYS_HASHING_2-POINTERS_SLIDINGWINDOW.py
+++ ARRAYS_HASHING_2-POINTERS_SLIDINGWINDOW.py
@@ -1,1 +1,20 @@
+Group Anagrams
+Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+Example 1:
+Input: strs = ["act","pots","tops","cat","stop","hat"]
+Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+Example 2:
+Input: strs = ["x"]
+Output: [["x"]]
+Example 3:
+Input: strs = [""]
+Output: [[""]]
 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
