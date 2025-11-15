eb8998c3f25df0372bf04ca7d0d1c6861ed7c6ba
Karthikeya Polakonda <98509169+karthikeyapolakonda@users.noreply.github.com>
2025-11-15 19:09:10 +0530

    Add Sudoku validation logic
    Implement validation logic for a 9x9 Sudoku board according to standard rules.

---
+++ 36. Valid Sudoku.py
@@ -0,0 +1,61 @@
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+-> Each row must contain the digits 1-9 without repetition.
+-> Each column must contain the digits 1-9 without repetition.
+-> Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in range(len(board)):
+            seen = set()
+            for i in range(len(board)):
+                if board[row][i] == '.':
+                    continue
+                if board[row][i] in seen:
+                    return False
+                seen.add(board[row][i])
+        for col in range(len(board)):
+            seen = set()
+            for i in range(len(board)):
+                if board[i][col] == '.':
+                    continue
+                if board[i][col] in seen:
+                    return False
+                seen.add(board[i][col])
+        for sq in range(9):
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (sq//3)*3 + i
+                    col = (sq%3)*3 + j
+                    if board[row][col] == '.':
+                        continue
+                    if board[row][col] in seen:
+                        return False   
+                    seen.add(board[row][col])            
+        return True
+#or
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in boxes[(r // 3, c // 3)]:
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                boxes[(r // 3, c // 3)].add(board[r][c])
+        return True
