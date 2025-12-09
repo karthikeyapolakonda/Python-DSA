42d23b88c8c5ae7bd38eb9bf925c9d907e5196b9
Karthikeya Polakonda <98509169+karthikeyapolakonda@users.noreply.github.com>
2025-12-09 15:56:29 +0530

    Create 424. Longest Repeating Character Replacement.py

---
+++ 424. Longest Repeating Character Replacement.py
@@ -0,0 +1,16 @@
+# Input: s = "ABAB", k = 2
+# Output: 4
+# Explanation: Replace the two 'A's with two 'B's or vice versa.
+  
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        longest = 0
+        l = 0
+        counts = [0] * 26
+        for r in range(len(s)):
+            counts[ord(s[r]) - 65] += 1
+            while (r - l + 1) - max(counts) > k:
+                counts[ord(s[l]) - 65] -= 1
+                l += 1
+            longest = max(longest, (r - l + 1))
+        return longest
